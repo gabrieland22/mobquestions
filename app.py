2b613a511ba069782dce2b18b29809c1bea65d83
Gabriel <gabrieland22@gmail.com>
2018-06-27 22:11:02 -0300

    'AulaTestes'

--- app.py
+++ app.py
@@ -5,17 +5,31 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from bson import json_util
 
-from config import MONGO_URI
+from config import MONGO_URI, MONGO_URI_TESTS, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 from auth import *
 
+import os
+import redis
 
-app = Flask(__name__)
-app.config['MONGO_URI'] = MONGO_URI
-app.config['DEBUG'] = True
+rcache = redis.Redis(
+            host=REDIS_HOST, 
+            port=REDIS_PORT,
+            password=REDIS_PASSWORD)
 
-app_context = app.app_context()
-app_context.push()
 
+def create_app(testing = False):
+    app = Flask(__name__)
+    if os.getenv('FLASK_TESTING') and os.getenv('FLASK_TESTING')=='1':
+        app.config['MONGO_URI'] = MONGO_URI_TESTS
+    else:
+        app.config['MONGO_URI'] = MONGO_URI
+    app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
+    app_context = app.app_context()
+    app_context.push()        
+    return app
+
+mongo = None
+app = create_app()
 mongo = PyMongo(app)
 
 col_users = mongo.db.users
@@ -44,12 +58,26 @@ def signin():
     else:
         return "Unauthorized", 401
 
-
 @app.route('/', methods=['GET'])
 @jwt_required
 def index():
     res = col_users.find({})
     return json_util.dumps(list(res)), 200
+
+@app.route('/cached_example', methods=['GET'])
+def questao_mais_legal_cacheada():    
+    if rcache and rcache.get('questao_legal'):
+        return rcache.get('questao_legal'), 200
+    else:
+        question = col_questions.find({'id': 'c14ca8e5-b7'})
+        if rcache:
+            rcache.set('questao_legal', json_util.dumps(question))
+    return json_util.dumps(question), 200
+
+@app.route('/not_cached_example', methods=['GET'])
+def questao_mais_legal():    
+    question = col_questions.find({'id': 'bc3b3701-b7'})
+    return json_util.dumps(question), 200
 
 
 @app.route('/refresh_token', methods=['GET'])
@@ -78,9 +106,13 @@ def token():
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json()
+    if 'password' not in data.keys() or 'username' not in data.keys():
+        return 'Dados insuficientes.', 400
     data['password'] = generate_password_hash(data['password'])
     col_users.insert_one(data)
-    return 'usuario ' + data['username'] + ' criado.', 201
+    del(data['password'])
+    return json_util.dumps(data), 201
+
 
 @app.route('/users/<username>', methods=['GET'])
 def get_user(username):
@@ -171,6 +203,123 @@ def get_question(question_id):
         return json_util.dumps(questao_encontrada), 200
     else : 
         return 'Questão '+ question_id + ' não foi encontrada.', 404
+
+##Atividade - 06
+@app.route('/v1/questions/<question_id>', methods=['POST'])
+@jwt_required
+def inserir_comentario_questao(question_id):
+    data = request.get_json()
+    print(col_questions.find_one())
+    if not question_id or not request or 'username' not in data or 'message' not in data:
+        return 'Dados não informados e/ou não encontrados para atualização', 401
+    else:
+        usuario_encontrado = col_users.find_one({"username" : data['username'] })
+        questao_encontrada = col_questions.find_one({'id' : question_id})
+        if not usuario_encontrado or not questao_encontrada:
+            return 'Usuário e/ou questão não encontrados', 403
+        else:
+            col_questions.update({'id' : question_id}, {'$set': {'comentarios' : data}})
+            return 'Comentário inserido com sucesso', 201
+
+##Atividade - 07
+@app.route('/v1/questions/search', methods=['GET'])
+def get_questoes():
+    disciplina = request.args.get('disciplina')
+    ano = request.args.get('ano')
+
+    if not disciplina and not ano:
+        return 'Dados enviados estão inválidos', 400
+    else:
+        where = {}
+        where['disciplina'] = int(disciplina)
+        where['ano'] = int(ano)
+        questions_encontradas = col_questions.find(where)
+        if questions_encontradas:
+            return json_util.dumps(list(questions_encontradas)), 200
+        else:
+            return 'Dados não encontrados', 404
+
+##Atividade - 08
+##Requerer token válido da atividade 03 e 06
+
+##Atividade - 09
+@app.route('/v1/questions/<question_id>/answer', methods=['POST'])
+@jwt_required
+def responder_questao(question_id):
+    data = request.get_json()
+    if not question_id or not request or 'resposta' not in data or 'username' not in data:
+        return 'Dados não informados e/ou não encontrados', 401
+    else:
+        questao = col_questions.find_one({'id' : question_id})
+        if not questao:
+            return 'Questão não encontrada', 403
+        else:
+            usuario_encontrado = col_users.find_one({'username' : data['username']})
+            if usuario_encontrado:
+                answerUser = {}
+                answerUser['id'] = question_id
+                answerUser['answer'] = data['resposta']
+                if 'questoes' not in usuario_encontrado:
+                    col_users.update({'username' : data['username']}, {'$set': {'questoes' : [answerUser]}})
+                else:
+                    col_users.update({'username' : data['username']}, {'$push': {'questoes' : answerUser}})
+
+                questoes_encontradas = col_users.find({'questoes': {'$ne' : None} },{'_id':0,'questoes.id' : 1})
+                lista = []
+                contador = int(0)
+                if questoes_encontradas:
+                    for x in questoes_encontradas:
+                        if len(x) > 0:     
+                            for i in x['questoes']:
+                                if i['id'] == question_id:
+                                    contador = contador + 1
+                col_questions.update({'id' : question_id}, {'$set': {'contador_respostas_recebida' : contador}})
+
+
+                if questao['resposta'] == data['resposta']:
+                    return 'Resposta correta', 201
+                else:
+                    return 'Resposta incorreta', 201
+            else: 
+                return 'Usuário não encontrado', 403
+
+##Atividade - 10
+@app.route('/v1/questions/answers', methods=['GET'])
+@jwt_required
+def get_respostas_questoes():
+    questoes_encontradas = col_users.find({'questoes': {'$ne' : None} },{'_id':0,'questoes' : 1})
+    
+    lista = []
+    if questoes_encontradas:
+        for x in questoes_encontradas:
+            if len(x) > 0:     
+                for i in x['questoes']:
+                    lista.append(i)
+    if len(lista) == 0:
+        return 'Nenhuma resposta encontrada', 404
+
+    return json_util.dumps(list(lista)), 201
+
+##Atividade - 11
+@app.route('/v1/featured_questions', methods=['POST'])
+@jwt_required
+def questoes_mais_respondidas():
+    questions = col_questions.find({'contador_respostas_recebida': {'$ne' : None}, 'contador_respostas_recebida' : {'$gt' : 0} })
+    print(json_util.dumps(list(questions)))
+    if questions and rcache:
+        rcache.set('questao_mais_respondida', json_util.dumps(list(questions)))
+        return 'Dados atualizados', 200
+    return 'Dados não atualizados', 403
+
+##Atividade - 12
+@app.route('/v1/featured_questions', methods=['GET'])
+@jwt_required
+def get_questoes_mais_respondidas():
+    if rcache and rcache.get('questao_mais_respondida'):
+        return rcache.get('questao_mais_respondida'), 200
+    return 'Não possui dados em cache',403
+
+
 
 
 
--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-MONGO_URI='mongodb://ada:chuvinha10@ds043358.mlab.com:43358/ubiqs'
+MONGO_URI='mongodb://gabriel:estadio02@ds213338.mlab.com:13338/aulacomputacaonuvem'
+MONGO_URI_TESTS = 'mongodb://gabriel:estadio02@ds115396.mlab.com:15396/aula_testes_automatizados'
+
+REDIS_HOST = 'redis-19805.c15.us-east-1-2.ec2.cloud.redislabs.com'
+REDIS_PORT = 19805
+REDIS_PASSWORD = 'TWizEVRLkIqSMslo4lqFtJaA9EpBWeLK'
+
+
